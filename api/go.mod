da509a9897b8e78bc9ab9810c28d81f2cfc9982d
Max Stanley <github@maxstanley.uk>
2021-03-20 14:00:05 +0000

    Issue #13: Added File-based datastore.

--- api/go.mod
+++ api/go.mod
@@ -6,8 +6,10 @@ require (
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.1 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.2.4 // indirect
@@ -18,4 +20,6 @@ require (
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.4 // indirect
 )
--- api/handler/search.go
+++ api/handler/search.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"net/http"
+
+	"github.com/maxstanley/fast_finder/datastore"
+	"github.com/maxstanley/fast_finder/models"
 )
 
 // NewSearchHandler returns the path pararemeter that is sent with the request.
@@ -11,5 +14,17 @@ func NewSearchHandler(c HandlerContext) (int, string) {
 		return http.StatusBadRequest, "no search parameter provided"
 	}
 
-	return http.StatusOK, searchParam
+	// Get datastore connection.
+	db := datastore.Connection()
+	var shortcut models.Shortcut
+	// Find the associated url for the searched keyword.
+	db.First(&shortcut, "keyword = ?", searchParam)
+
+	// If no record was returned, respond with not found.
+	if shortcut.Keyword == "" {
+		return http.StatusNotFound, ""
+	}
+
+	// Return the found Link for the associate keyword.
+	return http.StatusOK, shortcut.Link
 }
