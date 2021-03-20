70365ac186e88fb02cf0080306e7538dd7162085
Max Stanley <github@maxstanley.uk>
2021-03-20 00:01:38 +0000

    Issue #7: Added Signal Interrupt Handling

--- api/main.go
+++ api/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/maxstanley/fast_finder/handler"
 	"github.com/maxstanley/fast_finder/logger"
@@ -24,8 +30,34 @@ func main() {
 	// Set routes for not found.
 	r.NoRoute(handler.NewNotFoundHandler)
 
-	// Start router.
-	r.Start(":3000")
+	// Creates the HTTP Server.
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: r.Handler(),
+	}
+
+	// Starts the HTTP Server in a go routine so the interrupt signals can be
+	// handled.	 
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			logger.Info("HTTP Server Error: %s", err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	// Wait for a selected signal to interrupt the program.
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	signal := <-quit
+	logger.Info("%s Signal has been caught.", signal.String())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Attempt to gracefully shutdown the HTTP Server.
+	logger.Info("HTTP Server Shutting down.")
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Info("Server failed to shutdown gracefully: %s", err.Error())
+	}
 
 	logger.Info("Exiting Fast Finder API.")
 }
